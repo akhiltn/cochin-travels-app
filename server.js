9beaf5ab5938d13662f9ff0b1166000adb32e2f9
akhil <tnakhil@gmail.com>
2020-08-21 20:14:13 +0530

    trying to deploy to heroku

--- server.js
+++ server.js
@@ -1,16 +1,14 @@
-//Install express server
 const express = require('express');
-const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/cochin-travels-app'));
+app.use(express.static('./dist/cochin-travels-app'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/cochin-travels-app/index.html'));
+app.get('/*', function (req, res) {
+  res.sendFile('index.html', { root: 'dist/cochin-travels-app' }
+  );
 });
 
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
+console.log(`Running on port ${process.env.PORT || 8080}`)
