4a248223d15dc21eb1507e5d6a47240e0bf489d1
Akhil TN <35426085+akhiltn@users.noreply.github.com>
2026-01-25 07:48:23 +0000

    bugfix: contact us form transtack query

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -3,14 +3,19 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { Provider } from "./components/ui/provider.tsx";
 import { ColorModeProvider } from "./components/ui/color-mode.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./index.css";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ColorModeProvider defaultTheme="light">
-      <Provider>
-        <App />
-      </Provider>
+      <QueryClientProvider client={queryClient}>
+        <Provider>
+          <App />
+        </Provider>
+      </QueryClientProvider>
     </ColorModeProvider>
   </StrictMode>
 );
